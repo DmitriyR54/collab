59e363cb9b02dc7f8ecddfb638e3f54eb4da50f5
DmitriyR54 <45ryirtimd@gmail.com>
2023-08-18 18:31:04 +0300

    add the banner section

--- collab/src/pages/Home/Home.js
+++ collab/src/pages/Home/Home.js
@@ -2,7 +2,6 @@
 import 'modern-normalize/modern-normalize.css';
 import '../../assets/fonts/fonts.scss';
 import 'Src/styles/styles.scss';
-import './Home.scss';
 // components
 import { lazyLoadImages } from 'Components/lazyLoadImages/lazyLoadImages';
 import { hideLoadingScreen } from 'Sections/LoadingScreen/LoadingScreen';
@@ -12,6 +11,7 @@ import 'Sections/Intro/Intro.scss';
 import 'Sections/Partners/Partners';
 import 'Sections/Features/Features';
 import 'Sections/Testimonials/Testimonials.scss';
+import 'Sections/Banner/Banner.scss';
 import 'Sections/Footer/Footer.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
--- collab/src/sections/Features/Features.js
+++ collab/src/sections/Features/Features.js
@@ -1,28 +1,30 @@
 import './Features.scss';
 
-const sections = document.querySelectorAll('.features__description');
+setTimeout(() => {
+    const sections = document.querySelectorAll('.features__description');
 
-// observer function
-const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            const img = entry.target.querySelector('.features__description-img');
+    // observer function
+    const callback = (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const img = entry.target.querySelector('.features__description-img');
 
-            img.style.opacity = 1;
-            img.style.transform = 'translate3d(0, 0, 0)';
+                img.style.opacity = 1;
+                img.style.transform = 'translate3d(0, 0, 0)';
 
-            observer.unobserve(entry.target);
-        }
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    // observer options
+    const options = {
+        threshold: 0.25,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    sections.forEach((el) => {
+        observer.observe(el);
     });
-};
-
-// observer options
-const options = {
-    threshold: 0.25,
-};
-
-const observer = new IntersectionObserver(callback, options);
-
-sections.forEach((el) => {
-    observer.observe(el);
-});
+}, 50);
